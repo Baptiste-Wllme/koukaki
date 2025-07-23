1531f5230c14d78079eba66be950f434fa6d41a9
Baptiste Wllme <baptiste.willaume.pro@gmail.com>
2025-07-23 11:38:50 +0200

    thème enfant initialisé et début des fade

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -14,3 +14,10 @@ if ( get_stylesheet() !== get_template() ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
 }
+
+
+function foce_child_enqueue_styles() {
+    wp_enqueue_style( 'foce-parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'foce-child-style', get_stylesheet_directory_uri() . '/assets/css/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'foce_child_enqueue_styles' );
