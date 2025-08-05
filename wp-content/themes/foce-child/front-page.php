6fdcea61e2471b8909e75011af38333e7b45b2c6
Baptiste Wllme <baptiste.willaume.pro@gmail.com>
2025-08-05 17:17:26 +0200

    start responsive

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -7,7 +7,7 @@ get_header();
         <section class="banner fade-section-top">
             <?php get_template_part('partials/section', 'banner'); ?>
             
-            <div class="js-parallax logo-hidden">
+            <div class="js-parallax ">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo.png" class="hero__logo fade-section-bot float-logo " alt="logo Fleurs d'oranger & chats errants" />
             </div>
             
