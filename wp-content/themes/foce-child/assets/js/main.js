e4d07718d872fc88f9755293bb3f1287585d2abf
Baptiste Wllme <baptiste.willaume.pro@gmail.com>
2025-08-07 17:15:16 +0200

    remove fade-in-left/right

--- wp-content/themes/foce-child/assets/js/main.js
+++ wp-content/themes/foce-child/assets/js/main.js
@@ -1,15 +1,10 @@
-console.log("main.js est bien chargé !");
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const titles = document.querySelectorAll(".title-to-animate");
-  console.log("Titres trouvés :", titles);
-
-
+  
   const observer = new IntersectionObserver(
     (entries, obs) => {
       entries.forEach((entry) => {
-        console.log("voici l'entrée",entry);
         if (entry.isIntersecting) {
           entry.target.classList.add("title-visible");
           obs.unobserve(entry.target);
